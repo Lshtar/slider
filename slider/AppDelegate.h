fd897616e2a22341a9bdbc86b6c39567be5412b2
yujinhong <yjh90624@sina.com>
2018-07-31 09:23:17 +0800

    Initial Commit

---
+++ slider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  slider
+//
+//  Created by D on 2018/7/31.
+//  Copyright © 2018年 D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ slider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  slider
+//
+//  Created by D on 2018/7/31.
+//  Copyright © 2018年 D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
